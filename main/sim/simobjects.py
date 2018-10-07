349da9b855e7d8e91b4511b6d82c000f6b0bff67
Saptarshi Bhattacherya <bhat.bab@gmail.com>
2018-10-06 20:00:29 -0600

    fixed intersection

--- main/sim/simobjects.py
+++ main/sim/simobjects.py
@@ -4,6 +4,8 @@ Created on Sep 27, 2018
 @author: Babtu
 '''
 import pygame
+import random
+import time
 import os
 
 BLACK = (0, 0, 0)
@@ -14,9 +16,15 @@ BLUE = (0, 0, 255)
 
 img_folder = r'C:\Users\Babtu\eclipse-workspace\TrafficControl\main\sim'
 
+def rot_center(image, rect, angle):
+  """rotate an image while keeping its center"""
+  rot_image = pygame.transform.rotate(image, angle)
+  rot_rect = rot_image.get_rect(center=rect.center)
+  return rot_image, rot_rect
+
 class Car(pygame.sprite.Sprite):
 
-    def __init__(self, center, direction, screen):
+    def __init__(self, center, direction, speed, screen):
       pygame.sprite.Sprite.__init__(self)
       self.direction = direction
       self.screen = screen
@@ -24,60 +32,136 @@ class Car(pygame.sprite.Sprite):
       self.image.set_colorkey(WHITE)
       self.rect = self.image.get_rect()
       self.rect.center = center
+      self.speed = speed
+      self.position = []
+      self.orientation = 'horizontal' if direction in ['left', 'right'] else 'vertical'
+      if self.direction == 'left':
+        self.front = self.rect.midleft
+        self.back = self.rect.midright
+      elif self.direction == 'down':
+        self.front = self.rect.midbottom
+        self.back = self.rect.midtop
+      elif self.direction == 'up':
+        self.front = self.rect.midtop
+        self.back = self.rect.midbottom
+      else:
+        self.front = self.rect.midright
+        self.back = self.rect.midleft
 
       if self.direction == 'left':
         self.image = pygame.transform.rotate(self.image, 180)
+        self.front = self.rect.midleft
+        self.back = self.rect.midright
       elif self.direction == 'down':
         self.image = pygame.transform.rotate(self.image, 270)
+        self.front = self.rect.midbottom
+        self.back = self.rect.midtop
       elif self.direction == 'up':
         self.image = pygame.transform.rotate(self.image, 90)
+        self.front = self.rect.midtop
+        self.back = self.rect.midbottom
+      else:
+        self.front = self.rect.midright
+        self.back = self.rect.midleft
+    
+    def close(self, c):
+
+      if self.direction == c.direction:
+        if self.direction == 'right':
+          if abs(self.rect.midright[0] - c.rect.midleft[0]) < self.rect.h:
+            self.speed = 0
+            return True
+        elif self.direction == 'left':
+          if abs(self.rect.midleft[0] - c.rect.midright[0]) < self.rect.h:
+            self.speed = 0
+            return True
+        elif self.direction == 'up':
+          if abs(self.rect.midtop[1] - c.rect.midbottom[1]) < self.rect.h:
+            self.speed = 0
+            return True
+        else:
+          if abs(self.rect.midbottom[1] - c.rect.midtop[1]) < self.rect.h:
+            self.speed = 0
+            return True
+        return False
+      return False
+      
+
 
     def update(self):
-      if self.direction == 'right':
-        self.rect.x += 5
-      elif self.direction == 'left':
-        self.rect.x -= 5
-      elif self.direction == 'down':
-        self.rect.y += 5
-      elif self.direction == 'up':
-        self.rect.y -= 5
-      '''
-      if self.direction == 'right':
-        self.rect.x += 5
-        if self.rect.left > self.screen.get_size()[0] - self.rect.w:
-          self.direction = 'left'
-      elif self.direction == 'left':
-        self.rect.x -= 5
-        if self.rect.right < self.rect.w:
-          self.direction = 'right'
-      elif self.direction == 'down':
-        self.rect.y += 5
-        if self.rect.top > self.screen.get_size()[1] - self.rect.h:
-          self.direction = 'up'
-      elif self.direction == 'up':
-        self.rect.y -= 5
-        if self.rect.bottom < self.rect.h:
-          self.direction = 'down'
-      '''
+
+      show = True
+      end = None
+      for p in self.position:
+        if type(p) == Lane:
+          end = p.end
+      self.position = []
+      #check if car has left screen
+      if not self.screen.get_rect().colliderect(self.rect):
+        self.kill()
+        del self
+        show = False
+
+      if show:
+        if self.direction == 'right':
+          self.rect.x += self.speed
+        elif self.direction == 'left':
+          self.rect.x -= self.speed
+        elif self.direction == 'down':
+          self.rect.y += self.speed
+        elif self.direction == 'up':
+          self.rect.y -= self.speed
+
+            
+      
+
   
 class Intersection(pygame.sprite.Sprite):
-  def __init__(self, center, screen):
+  def __init__(self, width, height, center, screen):
     pygame.sprite.Sprite.__init__(self)
     self.screen = screen
     w, h = screen.get_size()
-    self.image = pygame.Surface((int(h * 0.2), int(h * 0.2)))#pygame.image.load(os.path.join(img_folder, r'redcar.png')).convert()
-    self.image.fill(BLACK)
+    self.image = pygame.image.load(os.path.join(img_folder,'Intersection.png'))
+    self.image = pygame.transform.scale(self.image, (width, height))
     self.image.set_colorkey(WHITE)
     self.rect = self.image.get_rect()
     self.rect.center = center
+    self.flow = 'horizontal'
+    self.cars = []
+  
+  def update(self, cars):
+    self.cars = []
+    for c in cars:
+      if self.rect.colliderect(c) or self.rect.contains(c):
+        self.cars.append(c)
+    
 
-class Road(pygame.sprite.Sprite):
-  def __init__(self, center, screen):
+class Lane(pygame.sprite.Sprite):
+  def __init__(self, width, height, direction, center, screen):
     pygame.sprite.Sprite.__init__(self)
     self.screen = screen
     w, h = screen.get_size()
-    self.image = pygame.Surface((int(h * 0.2), int(h * 0.2)))#pygame.image.load(os.path.join(img_folder, r'redcar.png')).convert()
-    self.image.fill(BLACK)
+    self.image = pygame.image.load(os.path.join(img_folder,'Lane.png'))
+    self.image = pygame.transform.scale(self.image, (width, height))
     self.image.set_colorkey(WHITE)
     self.rect = self.image.get_rect()
     self.rect.center = center
+    self.direction = direction
+
+    self.angle = 0
+    if direction == 'right':
+      self.image, self.rect = rot_center(self.image, self.rect, 90)
+      self.start = self.rect.midleft
+      self.end = self.rect.midright
+    elif direction == 'left':
+      self.image, self.rect = rot_center(self.image, self.rect, 90)
+      self.start = self.rect.midright
+      self.end = self.rect.midleft
+    elif direction == 'down':
+      self.start = self.rect.midtop
+      self.end = self.rect.midbottom
+    elif direction == 'up':
+      self.start = self.rect.midbottom
+      self.end = self.rect.midtop
+
+
--- main/simtest.py
+++ main/simtest.py
@@ -4,34 +4,38 @@ import os
 import random
 from sim.simobjects import *
 
+def reverse(flow):
+  return 'horizontal' if flow == 'vertical' else 'vertical'
+
 def _text_objects(text, font):
-  textSurface = font.render(text, True, BLACK)
+  textSurface = font.render(text, True, RED)
   return textSurface, textSurface.get_rect()
 
-
-def message_display(text):
-  largeText = pygame.font.Font('freesansbold.ttf',115)
+def message_display(text, center):
+  largeText = pygame.font.Font('freesansbold.ttf',70)
   TextSurf, TextRect = _text_objects(text, largeText)
-  TextRect.center = ((DISPLAY_WIDTH // 2, DISPLAY_HEIGHT // 2))
+  TextRect.center = (center)
   screen.blit(TextSurf, TextRect)
-
-  #time.sleep(1)
-
-def rect(x, y, w, h, c):
-  pygame.draw.rect(screen, c, [x, y, w, h])
 
 user32 = ctypes.windll.user32
 SCREEN_SIZE = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 COMBINED_SCREEN_SIZE = user32.GetSystemMetrics(78), user32.GetSystemMetrics(79)
 SECOND_SCREEN_SIZE = (COMBINED_SCREEN_SIZE[0] - SCREEN_SIZE[0], COMBINED_SCREEN_SIZE[1])
 
-ratio = 1.2
-DISPLAY_WIDTH, DISPLAY_HEIGHT = tuple([int(i // ratio) for i in list(SECOND_SCREEN_SIZE)])
+DISPLAY_MODE = "single" if COMBINED_SCREEN_SIZE == SCREEN_SIZE else "dual"
 
-x, y = ((COMBINED_SCREEN_SIZE[0] + SCREEN_SIZE[0] - DISPLAY_WIDTH) // 2, (COMBINED_SCREEN_SIZE[1] - DISPLAY_HEIGHT) // 2)
+ratio = 1.2
+if DISPLAY_MODE == "dual":
+  DISPLAY_WIDTH, DISPLAY_HEIGHT = tuple([int(i // ratio) for i in list(SECOND_SCREEN_SIZE)])
+  x, y = ((COMBINED_SCREEN_SIZE[0] + SCREEN_SIZE[0] - DISPLAY_WIDTH) // 2, (COMBINED_SCREEN_SIZE[1] - DISPLAY_HEIGHT) // 2)
+else:
+  DISPLAY_WIDTH, DISPLAY_HEIGHT = tuple([int(i // ratio) for i in list(SCREEN_SIZE)])
+  x, y = (SCREEN_SIZE[0] - DISPLAY_WIDTH) // 2, (SCREEN_SIZE[1] - DISPLAY_HEIGHT) // 2
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (x,y)
 
-ROAD_WIDTH = 250
+LANE_WIDTH = int(DISPLAY_WIDTH * 0.1)
+VERT_LANE_LENGTH = DISPLAY_HEIGHT // 2 - LANE_WIDTH
+HORZ_LANE_LENGTH = (DISPLAY_WIDTH // 2 - LANE_WIDTH)
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -46,13 +50,33 @@ pygame.init()
 screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 pygame.display.set_caption('Simulation')
 clock = pygame.time.Clock()
-objects = pygame.sprite.Group()
-carlist = []
-for i in range(4):
-  direction = random.choice(['up', 'down', 'right', 'left'])
-  carlist.append(Car((random.randint(0,DISPLAY_WIDTH),random.randint(0,DISPLAY_HEIGHT)), direction, screen))
-objects.add(*carlist)
-objects.add(Intersection(CENTER, screen))
+
+road = pygame.sprite.Group()
+cars = pygame.sprite.Group()
+intersection = Intersection(LANE_WIDTH * 2, LANE_WIDTH * 2, CENTER, screen)
+road.add(intersection)
+
+lanes = []
+lanes.append(Lane(LANE_WIDTH, VERT_LANE_LENGTH, 'down', ((DISPLAY_WIDTH - LANE_WIDTH) // 2, (DISPLAY_HEIGHT - VERT_LANE_LENGTH) // 2 - LANE_WIDTH), screen))
+lanes.append(Lane(LANE_WIDTH, VERT_LANE_LENGTH, 'up', ((DISPLAY_WIDTH + LANE_WIDTH) // 2, (DISPLAY_HEIGHT - VERT_LANE_LENGTH) // 2 - LANE_WIDTH), screen))
+lanes.append(Lane(LANE_WIDTH, HORZ_LANE_LENGTH, 'left', ((DISPLAY_WIDTH + HORZ_LANE_LENGTH) // 2 + LANE_WIDTH, (DISPLAY_HEIGHT - LANE_WIDTH) // 2), screen))
+lanes.append(Lane(LANE_WIDTH, HORZ_LANE_LENGTH, 'right', ((DISPLAY_WIDTH + HORZ_LANE_LENGTH) // 2 + LANE_WIDTH, (DISPLAY_HEIGHT + LANE_WIDTH) // 2), screen))
+
+lanes.append(Lane(LANE_WIDTH, VERT_LANE_LENGTH, 'up', ((DISPLAY_WIDTH + LANE_WIDTH) // 2, (DISPLAY_HEIGHT + VERT_LANE_LENGTH) // 2 + LANE_WIDTH), screen))
+lanes.append(Lane(LANE_WIDTH, VERT_LANE_LENGTH, 'down', ((DISPLAY_WIDTH - LANE_WIDTH) // 2, (DISPLAY_HEIGHT + VERT_LANE_LENGTH) // 2 + LANE_WIDTH), screen))
+lanes.append(Lane(LANE_WIDTH, HORZ_LANE_LENGTH, 'right', ((DISPLAY_WIDTH // 2 - LANE_WIDTH) // 2, (DISPLAY_HEIGHT + LANE_WIDTH) // 2), screen))
+lanes.append(Lane(LANE_WIDTH, HORZ_LANE_LENGTH, 'left', ((DISPLAY_WIDTH // 2 - LANE_WIDTH) // 2, (DISPLAY_HEIGHT - LANE_WIDTH) // 2), screen))
+road.add(*lanes)
+
+count = 0
+
+SPEED = 20
+
+l = random.choice([lanes[i] for i in [0, 2, 4, 6]])
+car = Car((DISPLAY_WIDTH // 2, DISPLAY_HEIGHT // 2), l.direction, SPEED, screen)
+#cars.add(car)
+
+
 
 while True:
   for event in pygame.event.get():
@@ -64,18 +88,51 @@ while True:
       if event.key == pygame.K_q:
         pygame.quit()
         quit()
-
-  objects.update()
   
-  screen.fill(BLUE)
-  coords = ((DISPLAY_WIDTH - ROAD_WIDTH) // 2, (DISPLAY_HEIGHT - ROAD_WIDTH) // 2)
-  #rect(coords[0], 0, ROAD_WIDTH, SCREEN_SIZE[1], BLACK)
-  #rect(0, coords[1], SCREEN_SIZE[0], ROAD_WIDTH, BLACK)
+  if count % int(100 / SPEED) == 0:
+    l = random.choice([lanes[i] for i in [0, 2, 4, 6]])
+    cars.add(Car(l.start, l.direction, SPEED, screen))
+  
+  for c in cars.sprites():
+    
+    c.speed = SPEED
+    
+    for d in cars.sprites():
+      if c.close(d) and Lane in [type(p) for p in c.position]:
+        c.speed = 0
+        d.speed = 0
+    
+    for d in road.sprites():
+      if d.rect.contains(c):
+        c.position.append(d)
+    
+    if c.rect.colliderect(intersection.rect):
+      if not intersection.rect.contains(c.rect):
+        if intersection.flow != c.orientation:
+          c.speed = 0
+        icars = []
+        for d in cars.sprites():
+          if intersection.rect.contains(d.rect):
+            icars.append(d)
+        if reverse(c.orientation) in [d.orientation for d in icars]:
+          c.speed = 0
+    
+  if count % int(200 / SPEED) == 0:
+    if not intersection.flow in [c.orientation for c in intersection.cars]:
+      intersection.flow = reverse(intersection.flow)
+  
 
-  objects.draw(screen)
+  road.update(cars.sprites())
+  cars.update()
+  
+  screen.fill(BLACK)
 
+  road.draw(screen)
+  cars.draw(screen)
+  
   pygame.display.update()
-
 
   clock.tick(60)
 
+  count += 1
+  
